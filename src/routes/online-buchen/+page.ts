b1987049b515808d809b333ddcd73d6d42818274
HLeonidas <leon.hueber@gmx.at>
2026-01-22 17:36:37 +0100

    fix sitemap and add redirects

---
+++ src/routes/online-buchen/+page.ts
@@ -0,0 +1,8 @@
+import { redirect } from '@sveltejs/kit';
+import { resolve } from '$app/paths';
+
+export const prerender = true;
+
+export const load = () => {
+	throw redirect(308, resolve('/buchen'));
+};
