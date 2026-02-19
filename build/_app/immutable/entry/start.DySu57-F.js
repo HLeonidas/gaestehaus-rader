7e6316f975e2ca22745e061addb4f801310f8254
HLeonidas <leon.hueber@gmx.at>
2026-02-19 18:58:14 +0100

    update tax price

---
+++ build/_app/immutable/entry/start.DySu57-F.js
@@ -0,0 +1,1 @@
+import{l as o,b as r}from"../chunks/BDz5PaHZ.js";export{o as load_css,r as start};
---
+++ build/_app/immutable/nodes/14.B1Ke71Xi.js
@@ -0,0 +1,1 @@
+import{r as e}from"../chunks/tMyF2QyT.js";import{r}from"../chunks/BAUSk3mR.js";const o=!0,t=()=>{throw e(308,r("/buchen"))},l=Object.freeze(Object.defineProperty({__proto__:null,load:t,prerender:o},Symbol.toStringTag,{value:"Module"}));export{l as universal};
